94d855876b4107e68bcfa4044b94c02e619e9946
hamperedwings41 <92867293+hamperedwings41@users.noreply.github.com>
2022-02-24 09:18:52 -0600

    Add files via upload

---
+++ code.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class code {
+
+	
+	//take 2 smallest frequencies
+	//add them together/
+	// add them to the bst
+	//do until there is only one 
+	
+	
+	
+	//                      10:*
+	//                     /  \
+	//                    5:1  5:2
+}
