47213dec12621dba2e2a7f32b8f4ea9859b56e15
nuttymannis <51046770+nuttymannis@users.noreply.github.com>
2022-03-01 09:19:37 -0600

    Add files via upload

--- code.java
+++ code.java
@@ -1,16 +1,83 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
+	public class code {
+	
+	Node root; 
+	
+	PriorityQueue<Node> queue = new PriorityQueue<Node>();
+	String bob;
+	Scanner in;
+	
+	public code () throws FileNotFoundException
+	{
+		root = null;
+		Scanner in = new Scanner( new File("tiny.txt"));
+		bob = in.next();
+	}
 
-public class code {
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		code c = new code();
+		
+		c.count();
+		
+		for(Node n : c.getCount()) {
+			System.out.println(n.data);
+		}
+		
+	}
+	
+	public void count() {
+		if(root != null)
+			count(root);
+	}
+	
+	private void count(Node t)
+	{
+		int m = 0;
+		String[] arr = bob.split("");
+		for(int x = 0; x<arr.length;x++)
+		{
+			char r=arr[x].charAt(0);
+			if(!queue.contains(t.data))
+			{
+				m = 1;
+				queue.add(new Node (r, m, null, null));	
+			}
+			else if(queue.contains(t.data))
+			{
+				m = t.cnt;
+				queue.remove(t.data);
+				queue.add(new Node (r, m++, null, null));	
+			}
+		}
+		System.out.println(queue);
 
+	}
 	
-	//take 2 smallest frequencies
-	//add them together/
-	// add them to the bst
-	//do until there is only one 
+	public PriorityQueue<Node> getCount(){
+		return queue;
+	}
 	
-	
-	
-	//                      10:*
-	//                     /  \
-	//                    5:1  5:2
+	class Node
+	{
+		char data;
+		Node left;
+		Node right;
+		int cnt;
+
+		public Node( char v, int c, Node l, Node r)
+		{
+			data = v;
+			left = l;
+			right = r;
+			cnt = c;
+		}
+		
+		public String toString()
+		{
+			return "" + data + " " + left + " " + right;
+		}
+	}
 }
---
+++ comp.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class comp {
+	public static void main(String[] args)
+	{
+		String t = "256 365 12 96 1111 9999 284 980";
+		String[] nums = t.split(" ");
+		ArrayList<num> list = new ArrayList<num>();
+		for( String v : nums)
+			 list.add(new num(v) );
+		Collections.sort(list );
+		System.out.println(list);
+	}
+	
+	 static class num implements Comparable<num>
+	{
+		String a;
+		int d = 0;
+		int y = 0;
+		public num (String v )
+		{
+			a = v;
+		}
+		public int compareTo( num other)
+		{
+			int b = Integer.parseInt(a);
+			int c = Integer.parseInt(other.a);
+			
+			while (b!= 0)
+			{
+				d = d + b % 10;
+				b = b/10;
+			}
+			while(c!= 0)
+			{
+				y = y + c % 10;
+				c = c/10;
+			}
+				if( d > y )
+					return 1;
+				if(d < y)
+					return -1;
+				if( d == y && b > c)
+					return 1;
+				return -1;
+		}
+		public String toString()
+		{
+			return a;
+		}
+	}
+}
+
