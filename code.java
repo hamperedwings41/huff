f6280498b21224e48828dcc2c2ab8de7838bb8c5
hamperedwings41 <92867293+hamperedwings41@users.noreply.github.com>
2022-03-07 08:26:28 -0600

    Add files via upload

--- code.java
+++ code.java
@@ -5,45 +5,69 @@ import java.util.*;
 	
 	Node root; 
 	
-	PriorityQueue<Node> count = new PriorityQueue<Node>();
+	PriorityQueue<Node> queue = new PriorityQueue<Node>();
 	String bob;
-	Scanner in = new Scanner(System.in);
+	Scanner in;
 	
 	public code () throws FileNotFoundException
 	{
 		root = null;
+		Scanner in = new Scanner( new File("tiny.txt"));
 		bob = in.next();
-	    Scanner in = new Scanner( new File("tiny.txt"));
+	}
+
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		code c = new code();
+		System.out.println("check 1");
+		System.out.println("check 2");
+		c.count(null);
+		for(Node n : c.getCount()) {
+			System.out.println("check 3");
+			System.out.println(n.data);
+		}
+		System.out.println("check 5");
+		
 	}
 	
-	public void count( String a, Node t)
+	
+	
+	public void count(Node t)
 	{
 		int m = 0;
+		queue.add(new Node('a',1,null,null));
 		String[] arr = bob.split("");
 		for(int x = 0; x<arr.length;x++)
 		{
 			char r=arr[x].charAt(0);
-			if(!count.contains(t.data))
+			for(Node n : queue)
 			{
-				m = 1;
-				count.add(new Node (r, m, null, null));	
-			}
-			else if(count.contains(t.data))
-			{
-				m = t.cnt;
-				count.remove(t.data);
-				count.add(new Node (r, m++, null, null));	
+				while(queue.peek()!=null)
+				{
+					if(queue.peek().data != r)
+						{
+						System.out.println("check");
+							queue.add(new Node (r, 1, null, null));	
+							
+						}
+					else if(queue.peek().data == r)
+						{
+							m = t.cnt;
+							queue.remove(t.data);
+							queue.add(new Node (r, m++, null, null));	
+						}
+				}
 			}
 		}
-		System.out.println(count);
+		System.out.println(queue);
 
 	}
 	
-	
-	
-	
-	
-	
+	public PriorityQueue<Node> getCount(){
+		System.out.println("check 4");
+		System.out.println(queue);
+		return queue;
+	}
 	
 	class Node
 	{
